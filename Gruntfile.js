6a6dd2623b25a59b5856ad434aab4f2eb656d570
Tanju Yıldız <ben@tanjuyildiz.com>
2015-12-06 22:57:21 +0200

    Added LESS compile task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,5 +17,65 @@ module.exports = function(grunt) {
     // Store your Package file so you can reference its specific data whenever necessary
     pkg: grunt.file.readJSON('package.json'),
 
+    /**
+     * Set project info
+     */
+    project: {
+      /**
+       * Project banner
+       * Dynamically appended to CSS/JS files
+       * Inherits text from package.json
+       */
+      banner: '/*!\n' +
+        ' * <%= pkg.name %>\n' +
+        ' * <%= pkg.title %>\n' +
+        ' * <%= pkg.url %>\n' +
+        ' * @author <%= pkg.author %>\n' +
+        ' * @version <%= pkg.version %>\n' +
+        ' * Copyright <%= pkg.copyright %>. <%= pkg.license %> licensed.\n' +
+        ' */\n',
+
+      /**
+       * Project Paths
+       */
+      app: {
+        base: "app",
+        components: "<%= app.base %>/components"
+        scripts: "<%= app.base %>/scripts",
+        styles: "<%= app.base %>/styles",
+        templates: "<%= app.base %>/templates"
+      },
+      build: {
+        base: "build"
+      },
+      dist: {
+        base: "dist",
+        scripts: "<% dist.base %>/scripts",
+        styles: "<% dist.base %>/styles",
+        templates: "<% dist.base %>"
+      }
+    },
+
+    /**
+     * Compile LESS files to CSS
+     *
+     * @url https://github.com/gruntjs/grunt-contrib-less
+     */
+    less: {
+      options: {
+        banner: '<%= .project.banner %>'
+      },
+      development: {
+        options: {
+          paths: ['<%= .project.app.styles %>'],
+          yuicompress: false
+        },
+        files: {
+          '<%= .project.dist.styles %>/style.css': '<%= .project.app.styles %>/style.less'
+        }
+      }
+    },
+
+
   });
 }
